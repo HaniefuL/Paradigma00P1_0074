3644c90afa187956ef9fce5042f526ea763858c7
HaniefuL <haniefulgeofarry@gmail.com>
2024-05-20 19:58:26 +0700

    menambahkan  return 0 untuk menandakan  program selesai

--- Pewarisan/pewarisan.cpp
+++ Pewarisan/pewarisan.cpp
@@ -18,3 +18,27 @@ class orang {
         }
 };
 
+class pelajar : public orang {
+    public: 
+        string sekolah;
+
+        pelajar(string pNama, string pSekolah): orang(pNama), sekolah(pSekolah) {
+            cout << "Pelajar dibuat\n" << endl;
+        }
+
+        ~pelajar() {
+            cout << "Pelajar dihapus\n" << endl;
+        }
+
+        string perkenalan() {
+            return "Halo, nama saya " + nama + " dari sekolah " + sekolah + "\n \n";
+        }
+};
+
+int main() {
+    pelajar siswa1("Andi Aksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+}
